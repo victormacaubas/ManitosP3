07c74c7c5a6d73093ea6f19aa372f850da6df930
Victor Macaubas <victor@macaubas.com>
2022-11-01 16:27:04 -0300

    Revisão na classe produto

--- src/Product.java
+++ src/Product.java
@@ -4,20 +4,9 @@ public class Product implements Comparable<Product> {
     private double price;
     private int productStock;
 
-    public Product(String id, String name, double price, int productStock) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.productStock = productStock;
-    }
-
     public Product(String id, String name) {
         this.id = id;
         this.name = name;
-    }
-
-    public Product(String id) {
-        this.id = id;
     }
 
     public String toString() {
