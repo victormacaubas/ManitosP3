046e8a925253ef88da24a1520937b2c1762b6d70
Dtssouza <danilo.00000829790@unicap.br>
2022-11-04 09:26:40 -0300

    danilo  commit

---
+++ src/DoublyNode.java
@@ -0,0 +1,42 @@
+public class DoublyNode<T extends Comparable<T>> {
+
+    private DoublyNode<T> prev;
+    private DoublyNode<T> next;
+    private T info;
+
+    
+    public DoublyNode(T info) {
+        this.info = info;
+    }
+
+
+    public DoublyNode<T> getPrev() {
+        return prev;
+    }
+
+
+    public void setPrev(DoublyNode<T> prev) {
+        this.prev = prev;
+    }
+
+
+    public DoublyNode<T> getNext() {
+        return next;
+    }
+
+
+    public void setNext(DoublyNode<T> next) {
+        this.next = next;
+    }
+
+
+    public T getInfo() {
+        return info;
+    }
+
+
+    public void setInfo(T info) {
+        this.info = info;
+    }
+
+}
