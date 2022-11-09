91b7b698c1c6a7f35532ef15dd8b2518f45039c8
Luís Gustavo <lgpereira2@outlook.com>
2022-11-09 19:52:59 -0300

    Variaveis e o metodo

--- src/Queue.java
+++ src/Queue.java
@@ -1,3 +1,11 @@
-public class Queue {
-    
+public class Queue<T> {
+
+    private DoublyNode<T> first;
+    private DoublyNode<T> last;
+    private int size;
+    private int nodeCount;
+
+    public Queue(int size) {
+        this.size = size;
+    }
 }
