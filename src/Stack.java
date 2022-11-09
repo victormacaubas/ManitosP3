c3fff39b7f7087963d4975c974b578c2add750b8
Luís Gustavo <lgpereira2@outlook.com>
2022-11-09 20:03:33 -0300

    methods

--- src/Stack.java
+++ src/Stack.java
@@ -2,6 +2,12 @@ public class Stack<T> {
 
     private DoublyNode<T> top;
 
-    public boolean isEmpty() {
+    public boolean isEmpty() {}
+    
+    public void push (T data) {}
+
+    public boolean isFull() {}
+
+    public T pop() {}
     
 }
