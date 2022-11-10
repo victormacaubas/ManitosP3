4b71df3b4e4108d83e8d8dedfb3fc5bc8feaba5f
Luís Gustavo <lgpereira2@outlook.com>
2022-11-09 21:06:43 -0300

    Implementação dos metodos

--- src/Stack.java
+++ src/Stack.java
@@ -2,12 +2,33 @@ public class Stack<T> {
 
     private DoublyNode<T> top;
 
-    public boolean isEmpty() {}
-    
-    public void push (T data) {}
+    public boolean isEmpty() {
+        if (this.top == null) {
+            return true;
 
-    public boolean isFull() {}
+        }
 
-    public T pop() {}
+        return false;
+    }
     
+    public void push (T data) {
+        
+        DoublyNode<T> newNode = new DoublyNode<>(data);
+
+        newNode.setNext(this.top);
+        this.top = newNode;
+    }
+
+    public boolean isFull() {
+        return false;
+    }
+
+    public T pop() {
+        DoublyNode<T> aux = this.top;
+        this.top = this.top.getNext();
+
+        return aux.getInfo();
+
+    }
+
 }
